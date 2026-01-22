0c2701a74bcd0b80b7f2e6d31b1fac5ad476322b
MiguelSLz <MiguelSoria348@gmail.com>
2026-01-22 17:17:20 -0300

    commit 1

---
+++ src/corrente_publisher.cpp
@@ -0,0 +1,13 @@
+/*
+22/01/2026 - Miguel Sória da Luz
+código responsavel por:
+fazer a leitura dos valores brutos do adc provenientes da esp, via comunicação serial;
+calibração desses valores via regressão linear para se encontrar as correntes;
+publicar em topico do ros2 esses dados das correntes eletricas;
+*/
+
+// bibliotecas comunicação serial
+#include <fcntl.h>
+#include <termios.h>
+#include <unistd.h>
+
